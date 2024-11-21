47e94886f572828c12dc67f17d9315b6cb62d7e4
Ahmad Faid Ismat <afismat1@sheffield.ac.uk>
2024-11-21 12:33:49 +0000

    Add data cleaning

--- data_preparation.py
+++ data_preparation.py
@@ -1,3 +1,8 @@
 import pandas as pd
 df = pd.read_csv("U:\data-science-project\Iris.csv")
 print(df.head())
+
+df = df.dropna()
+df.columns = [col.strip().lower().replace(' ', '_') for col in df.columns]
+print("\nData after cleaning:")
+print(df.head())
